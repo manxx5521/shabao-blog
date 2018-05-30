340cbc4cc61d20fa056667e822a9e6b5e94e9ebb
xv <xv@xv-PC>
2018-05-30 22:37:59 +0800

    添加https配置

--- src/main/java/com/xiaoshabao/blog/BlogApplication.java
+++ src/main/java/com/xiaoshabao/blog/BlogApplication.java
@@ -1,8 +1,16 @@
 package com.xiaoshabao.blog;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 import com.xiaoshabao.base.BaseApplication;
 
@@ -11,10 +19,45 @@ import com.xiaoshabao.base.BaseApplication;
 @SpringBootApplication
 public class BlogApplication extends BaseApplication {
 
+	@Value("${http.port}")
+	private Integer httpPort;
+	@Value("${server.port}")
+	private Integer sslPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
-	
+	/**
+	 * 初始化配置tomcat
+	 */
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// 添加http
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	// 配置http
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setRedirectPort(sslPort);
+		return connector;
+	}
 
 }
