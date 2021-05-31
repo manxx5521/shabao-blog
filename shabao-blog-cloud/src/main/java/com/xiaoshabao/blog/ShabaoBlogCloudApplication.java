6fe454188db65a5553b214239cd0c3ed7dd9c4b1
maxx <maxx@yonyou.com>
2021-06-01 02:48:54 +0800

    11

---
+++ shabao-blog-cloud/src/main/java/com/xiaoshabao/blog/ShabaoBlogCloudApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoshabao.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShabaoBlogCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShabaoBlogCloudApplication.class, args);
+	}
+
+}
